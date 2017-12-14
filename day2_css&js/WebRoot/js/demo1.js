925bd54164045f73fbd2767894b311c159eb7fb7
Scyzin <32444500+Scyzin@users.noreply.github.com>
2017-12-14 18:40:40 +0800

    day2 css&js

---
+++ day2_css&js/WebRoot/js/demo1.js
@@ -0,0 +1,23 @@
+	//变量
+		var x = 5;
+		x = 'javascript';	
+		var y = "hello";
+		var b = true; 
+		alert(y);
+	//数据类型,是为对象
+		alert(typeof b.toString());
+		alert(typeof parseInt(x));
+			
+		var obj = new Object();
+		alert(typeof obj);
+		
+		var x = 5; 
+		var y ="123";
+		alert(x+y);
+		alert(x-y);
+
+		if(9){
+			alert("true--");
+		}else{
+			alert("false--");
+		}
